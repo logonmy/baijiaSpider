4de778b73d0a02215659fbc7497721af086618e6
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-12-01 09:54:29 +0800

    百家号存入MySQL或Redis

---
+++ baijiaUserToDB.py
@@ -0,0 +1,70 @@
+# coding:utf-8
+
+from pymysql import connect
+import redis
+import json
+import re
+
+class DB(object):
+
+    def __init__(self):
+        self.redis_cli = redis.Redis(host="111.230.136.142", port=6480, db=5, password="zy79117911#",
+                                     decode_responses=True)
+        self.db = connect(host="gz-cdb-fc6eprur.sql.tencentcdb.com", port=61979, db="zhan_db", user="root", password="zy79117911#",
+                          charset="utf8")
+        self.cursor = self.db.cursor()
+
+    def save_to_redis(self):
+        # SELECT id,userId,mid FROM HZ1
+        sql = "SELECT userName,userId,source_url from baiduUser9"
+        self.cursor.execute(sql)
+        data = self.cursor.fetchall()
+        print(type(data))
+        print('===== start =====')
+        num = 0
+        for d in data:
+            print('从mysql把户主数据导入redis数据库中，正在导入第', num + 1, '个户主')
+            # self.redis_cli2.sadd('baiduUserId',d[0])
+            d = list(d)
+            user_dict = {}
+            user_dict['userName'] = d[0]
+            user_dict['userId'] = d[1]
+            user_dict['source_url'] = d[2]
+            user_dict['offset'] = 0
+            user_dict['end_time'] = 1514736000
+            #
+            self.redis_cli.lpush('baijia_2018_history_data', user_dict)
+            num += 1
+        print('over', num)
+
+    def save_to_mysql(self):
+        try:
+            all_data = self.redis_cli.lrange('baijia_newUsers', 0, -1)
+        except Exception as e:
+            print("出错")
+            return
+        # print("获取到值了", type(all_data))
+
+        k = 0
+        try:
+            for i in all_data:
+                i = json.loads(re.sub('\'', '\"', i))
+                sql = "insert into baiduUser7 (userName,userId,source_url,last_time) values ('%s', '%s', '%s', '%s')" % (
+                i['userName'], i['userId'], i['source_url'], i['end_time'])
+                self.cursor.execute(sql)
+                k = k + 1
+                if (k > 1000):
+                    self.db.commit()
+                    print("插入成功！")
+                    k = 0
+
+            self.db.commit()
+        except Exception as e:
+            print("sql语句出错", e)
+            return
+        print("完成导入")
+
+if __name__ == '__main__':
+    save_db = DB()
+    save_db.save_to_redis()
+    save_db.save_to_mysql()
