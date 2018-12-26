63495cdc06787a8a6090d46cc2e5c4d3a9a449ab
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-12-26 20:49:44 +0800

    百家号文章内容分类整理

--- articleCategory.py
+++ articleCategory.py
@@ -14,8 +14,7 @@ import redis
 class articleCategory(object):
     def __init__(self):
         try:
-            self.redis_cli2 = redis.Redis(host="secret", port=6379, db=1, password="secret",
-                                      decode_responses=True)
+            self.redis_cli2 = redis.Redis(host="secret", port=6379, db=1, password="secret",decode_responses=True)
         except:
             print("连接redis数据库失败")
             return
